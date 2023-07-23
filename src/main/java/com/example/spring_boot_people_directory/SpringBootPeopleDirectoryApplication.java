d580ef55624b8e4b2699dd7c0fb1786ba77f2eea
Aleksandr Straton <houka1337@gmail.com>
2023-07-23 10:05:34 +0300

    spring initializr

---
+++ src/main/java/com/example/spring_boot_people_directory/SpringBootPeopleDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_people_directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPeopleDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPeopleDirectoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_boot_people_directory/SpringBootPeopleDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_people_directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPeopleDirectoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
